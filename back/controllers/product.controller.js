5db569fe39c1b4177f11c4593d579abc95148381
tharushaudana <tharushaudana519@gmail.com>
2025-06-04 23:03:36 +0530

    Implement product retrieval by ID and enhance database schema for Customers and CustomerAddresses

--- back/controllers/product.controller.js
+++ back/controllers/product.controller.js
@@ -1,4 +1,4 @@
-const { getProducts } = require('../services/product.service');
+const { getProducts, getProductById } = require('../services/product.service');
 
 exports.getProducts = async (req, res) => {
     try {
@@ -9,3 +9,22 @@ exports.getProducts = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
+
+exports.getProductById = async (req, res) => {
+    const productId = parseInt(req.params.id, 10);
+    
+    if (isNaN(productId)) {
+        return res.status(400).json({ error: 'Invalid product ID' });
+    }
+
+    try {
+        const product = await getProductById(productId);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.error('Error in getProductById:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
