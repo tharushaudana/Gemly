064abb24966fa4685acdf20dc21a762f5963f606
tharushaudana <tharushaudana519@gmail.com>
2025-06-04 16:57:32 +0530

    Add CORS middleware and update package dependencies

--- back/app.js
+++ back/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const productRoutes = require('./routes/product.route');
 const filterRoutes = require('./routes/filters.route');
+const cors = require('cors');
 
 const app = express();
+app.use(cors()); // allow all origins by default
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/products', productRoutes);
 app.use('/filters', filterRoutes);
